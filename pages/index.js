90b98e6cb7b9704944e09e4319deeb50d0c7a1db
harshit <harshit211997@gmail.com>
2023-03-29 21:13:25 +0530

    Remove top margin

--- pages/index.js
+++ pages/index.js
@@ -21,17 +21,6 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-          Hi. I'm <b>Harshit</b>. An Engineer. 
-          I am also the author of this blog and an ex game developer. 
-          You can contact me on <a href="https://twitter.com/harshit211997">twitter</a>
-        </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
